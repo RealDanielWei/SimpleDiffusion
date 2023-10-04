4e4c7a923603fc4b3f82157386c76b992102aba0
Daniel Wei <teslawell@gmail.com>
2023-10-03 21:17:47 -0700

    Enable inpainting

--- StableDiffusionSystem.py
+++ StableDiffusionSystem.py
@@ -1,46 +1,84 @@
 from dataclasses import dataclass
 import torch
 from transformers import CLIPTextModel, CLIPTokenizer
-from diffusers import StableDiffusionPipeline, AutoencoderKL, UNet2DConditionModel, PNDMScheduler
+from diffusers import StableDiffusionPipeline, StableDiffusionInpaintPipeline, AutoencoderKL, UNet2DConditionModel, PNDMScheduler
+from PIL import Image
 
 @dataclass
 class StableDiffusionConfig:
     device = "cpu"
-    model_name = "runwayml/stable-diffusion-v1-5"  #name of model used for inference
     cache_dir  = "./.cache"   #path for caching
-    num_inference_steps = 25  # Number of denoising steps
     guidance_scale = 7.5  # Scale for classifier-free guidance
-    seed = 0
-    save_memory = True
 
 class StableDiffusionSystem:
     def __init__(self, config : StableDiffusionConfig):
         self.config = config
-        self.generator = torch.manual_seed(self.config.seed)
-        self.sdpipeline = StableDiffusionPipeline(
-            vae = AutoencoderKL.from_pretrained(self.config.model_name, subfolder="vae", cache_dir = self.config.cache_dir).to(self.config.device),
-            text_encoder = CLIPTextModel.from_pretrained(self.config.model_name, subfolder="text_encoder", cache_dir = self.config.cache_dir).to(self.config.device),
-            tokenizer = CLIPTokenizer.from_pretrained(self.config.model_name, subfolder="tokenizer", cache_dir = self.config.cache_dir),
-            unet = UNet2DConditionModel.from_pretrained(self.config.model_name, subfolder="unet", cache_dir = self.config.cache_dir).to(self.config.device),
-            scheduler = PNDMScheduler.from_pretrained(self.config.model_name, subfolder="scheduler"),
-            safety_checker = None,
-            feature_extractor=None,
-            requires_safety_checker=False,
-        )
-        if self.config.save_memory:
-            self.sdpipeline.enable_attention_slicing()
-            self.sdpipeline.enable_vae_slicing()
-            self.sdpipeline.enable_vae_tiling()
-            if self.config.device == "cuda":
-                self.sdpipeline.enable_xformers_memory_efficient_attention()
-        
+        self.generator = torch.manual_seed(0)
+        self.model_name = ""
+        self.sdpipeline = None
+        self.inpainting_pipeline = None
+
     def set_seed(self, seed):
         self.generator.manual_seed(seed)
 
-    def textToImage(self, prompt, negative_prompt, steps):
+    def load_components(self):
+        self.vae = AutoencoderKL.from_pretrained(self.model_name, subfolder="vae", cache_dir = self.config.cache_dir).to(self.config.device)
+        self.vae.enable_tiling()
+        self.vae.enable_slicing()
+        self.text_encoder = CLIPTextModel.from_pretrained(self.model_name, subfolder="text_encoder", cache_dir = self.config.cache_dir).to(self.config.device)
+        self.unet = UNet2DConditionModel.from_pretrained(self.model_name, subfolder="unet", cache_dir = self.config.cache_dir).to(self.config.device)
+        self.tokenizer = CLIPTokenizer.from_pretrained(self.model_name, subfolder="tokenizer", cache_dir = self.config.cache_dir)
+        self.scheduler = PNDMScheduler.from_pretrained(self.model_name, subfolder="scheduler", cache_dir = self.config.cache_dir)
+
+    def textToImage(self, model_name, prompt, negative_prompt, steps):
+        if model_name != self.model_name:
+            self.model_name = model_name
+            self.load_components()
+            self.sdpipeline = StableDiffusionPipeline(
+            vae = self.vae,
+            text_encoder = self.text_encoder,
+            tokenizer = self.tokenizer,
+            unet = self.unet,
+            scheduler = self.scheduler,
+            safety_checker = None,
+            feature_extractor = None,
+            requires_safety_checker = False,
+        )
+            self.sdpipeline.enable_attention_slicing()
         return self.sdpipeline(
             prompt = prompt,
             negative_prompt = negative_prompt,
+            num_inference_steps = steps,
+            guidance_scale = self.config.guidance_scale,
+            generator = self.generator,
+        ).images
+
+    def inpainting(self, model_name, prompt, negative_prompt, input_img, mask_img, steps):
+        if model_name != self.model_name:
+            self.model_name = model_name
+            self.load_components()
+            self.inpainting_pipeline = StableDiffusionInpaintPipeline(
+            vae = self.vae,
+            text_encoder = self.text_encoder,
+            tokenizer = self.tokenizer,
+            unet = self.unet,
+            scheduler = self.scheduler,
+            safety_checker = None,
+            feature_extractor = None,
+            requires_safety_checker = False,
+            )
+        self.inpainting_pipeline.enable_attention_slicing()
+
+        input_img = Image.fromarray(input_img)
+        input_img.resize((256, 256))
+        mask_img = Image.fromarray(mask_img)
+        mask_img.resize((128, 128))
+
+        return self.inpainting_pipeline(
+            prompt = prompt,
+            negative_prompt = negative_prompt,
+            image = input_img,
+            mask_image = mask_img,
             num_inference_steps = steps,
             guidance_scale = self.config.guidance_scale,
             generator = self.generator,
--- ui.py
+++ ui.py
@@ -7,8 +7,8 @@ sd = StableDiffusionSystem(sd_config)
 def set_seed(seed):
     sd.generator.manual_seed(seed)
 
-def text2img_gradio(prompt, negative_prompt, steps, progress=gr.Progress(track_tqdm=True)):
-    return sd.textToImage(prompt=prompt, negative_prompt = negative_prompt, steps = steps)[0]
+def text2img_gradio(model_name, prompt, negative_prompt, steps, progress=gr.Progress(track_tqdm=True)):
+    return sd.textToImage(model_name = model_name, prompt = prompt, negative_prompt = negative_prompt, steps = steps)[0]
 
 img_list = []
 def transfer(img):
@@ -23,9 +23,15 @@ def count_down_to_zero(x):
         return int(x) - 1
     return int(x)
 
+def getMask(img : gr.ImageMask):
+    return img["mask"]
+
+def inpainting_gradio(model_name, prompt, negative_prompt, img, steps, progress=gr.Progress(track_tqdm=True)):
+    return sd.inpainting(model_name = model_name, prompt = prompt, negative_prompt = negative_prompt, input_img = img["image"], mask_img = img["mask"], steps = steps)[0]
+
 with gr.Blocks() as demo:
     gr.Markdown("SimpleDiffusion UI")
-    model_name = gr.Dropdown(value = sd.config.model_name, choices = [sd.config.model_name], label= "Model")
+    model_name = gr.Dropdown(value = "runwayml/stable-diffusion-v1-5", choices = ["runwayml/stable-diffusion-v1-5", "runwayml/stable-diffusion-inpainting"], label= "Model")
     with gr.Tab("Text2Image"):
         prompt = gr.Textbox(label= "Positive Prompt")
         negative_prompt = gr.Textbox(label= "Negative Prompt")
@@ -38,12 +44,17 @@ with gr.Blocks() as demo:
         gallery = gr.Gallery()
         clear_button = gr.ClearButton([gallery])
         generate_button.click(set_seed, inputs = seed, outputs = None).then(minus_one, inputs = total_requested_image_num, outputs = image_to_generate)
-        image_to_generate.change(text2img_gradio, inputs=[prompt, negative_prompt, steps], outputs = img_out).then(transfer, inputs = img_out, outputs = gallery).then(count_down_to_zero, inputs = image_to_generate, outputs = image_to_generate)
+        image_to_generate.change(text2img_gradio, inputs=[model_name, prompt, negative_prompt, steps], outputs = img_out).then(transfer, inputs = img_out, outputs = gallery).then(count_down_to_zero, inputs = image_to_generate, outputs = image_to_generate)
 
-    with gr.Tab("Flip Image"):
+    with gr.Tab("Inpainting"):
         with gr.Row():
-            image_input = gr.Image()
+            image_input = gr.ImageMask()
             image_output = gr.Image()
-        image_button = gr.Button("Flip")
+        prompt = gr.Textbox(label= "Prompt")
+        negative_prompt = gr.Textbox(label= "Negative Prompt")
+        steps = gr.Slider(label= "Number of Steps", minimum = 1, maximum = 30, value = 20, step = 1)
+        seed = gr.Slider(label= "Random seed", minimum = 1, maximum = 2147483647, value = 666, step = 1)
+        inpaint_button = gr.Button("Inpaint")
+        inpaint_button.click(set_seed, inputs = seed, outputs = None).then(inpainting_gradio, inputs = [model_name, prompt, negative_prompt, image_input, steps], outputs = image_output)
 
 demo.queue().launch()
